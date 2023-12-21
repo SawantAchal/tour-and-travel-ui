7aa28f7801de4019807630e4c85b600364d3fb4b
SawantAchal <achals1718@gmail.com>
2023-12-21 19:34:47 +0530

    made changes in all this files

---
+++ src/Components/MenuItems.js
@@ -0,0 +1,34 @@
+const MenuItems = [
+    {
+        title:"Home",
+        url:"/",
+        className:"nav-links",
+        icon:'FaHome'
+    },
+    {
+        title:"About",
+        url:"/about",
+        className:"nav-links",
+        icon:'FaInfoCircle'
+    },
+    {
+        title:"Service",
+        url:"/service",
+        className:"nav-links",
+        icon:'FaBriefcase'
+    },
+    {
+        title:"Contact",
+        url:"/contact",
+        className:"nav-links",
+        icon:'FaAddressBook'
+    },
+    {
+        title:"SignUp",
+        url:"/signup",
+        className:"nav-links-mobile"
+    }
+
+]
+
+export  {MenuItems}
