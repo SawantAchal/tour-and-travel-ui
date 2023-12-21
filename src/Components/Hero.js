aa3b09a7eae21b0f4604e374988f93740706053b
SawantAchal <achals1718@gmail.com>
2023-12-21 21:28:02 +0530

    add props in hero.js to make page dyamic

---
+++ src/Components/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../Assets/CSS/Hero.css'
+import img1 from '../Assets/images/hero1.jpg'
+import { Link } from 'react-router-dom'
+
+const Hero = (props) => {
+  return (
+    <>
+        <section className={props.cName}>
+            <img alt='main img' src={props.heroImg}/>
+            <div className='hero-text'>
+                <h1>{props.Htitle}</h1>
+                <p>{props.Htext}</p>
+                <Link className={props.btnClass} to={props.url}>{props.btmText}</Link>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Hero
